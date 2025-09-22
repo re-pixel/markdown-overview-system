3ad238ffe2b9ba75e249d8b0093a009ccc97be0d
re-pixel <relja.brdar@gmail.com>
2025-09-22 10:22:25 +0200

    feat: setting up database and server connection

--- backend-go/cmd/server/main.go
+++ backend-go/cmd/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+
+	"backend-go/internal/db"
+)
 
 func main() {
-	fmt.Println("Go backend placeholder")
+	err := godotenv.Load("../../backend-go.env")
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
+	conn, err := db.Connect()
+
+	if err != nil {
+		log.Fatalf("database connection error: %v", err)
+	}
+	defer conn.Close(context.Background())
+	fmt.Println("Connected to NeonDB")
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Server is running!"))
+	}).Methods("GET")
+
+	// Port iz .env
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("🚀 Server started on http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
---
+++ backend-go/internal/db.go
@@ -0,0 +1,58 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func Connect() (*pgx.Conn, error) {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		return nil, fmt.Errorf("database_url is not set")
+	}
+
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return conn, nil
+}
+
+// Template from neondb site
+
+// err := godotenv.Load("../../backend-go.env")
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+// 	connStr := os.Getenv("DATABASE_URL")
+// 	conn, err := pgx.Connect(context.Background(), connStr)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer conn.Close(context.Background())
+// 	_, err = conn.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS playing_with_neon(id SERIAL PRIMARY KEY, name TEXT NOT NULL, value REAL);")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	_, err = conn.Exec(context.Background(), "INSERT INTO playing_with_neon(name, value) SELECT LEFT(md5(i::TEXT), 10), random() FROM generate_series(1, 10) s(i);")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	rows, err := conn.Query(context.Background(), "SELECT * FROM playing_with_neon")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var id int32
+// 		var name string
+// 		var value float32
+// 		if err := rows.Scan(&id, &name, &value); err != nil {
+// 			panic(err)
+// 		}
+// 		fmt.Printf("%d | %s | %f\n", id, name, value)
+// 	}
